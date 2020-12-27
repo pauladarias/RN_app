00cd87a583c70ce6e696b0f8696e13e168be4388
pauladarias <pauladarias@gmail.com>
2020-12-27 09:48:17 +0000

    Add flex box  model to BoxScreen style

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,19 +3,37 @@ import { Text, View, StyleSheet, TextInput } from "react-native"
 
 const BoxScreen = () => {
   return <View style={styles.viewStyle}>
-    <Text style={styles.textStyle}>Box Screen</Text>
+    <Text style={styles.textOneStyle}>Child 1</Text>
+    <Text style={styles.textTwoStyle}>Child 2</Text>
+    <Text style={styles.textThreeStyle}>Child 3</Text>
   </View>
 }
 
 const styles = StyleSheet.create({
   viewStyle: {
     borderWidth: 7,
-    borderColor: "black"
+    borderColor: "black",
+    height: 200,
+
   },
-  textStyle: {
+  textOneStyle: {
     borderWidth: 3,
     borderColor: "red",
-    margin: 20
+    flex: 1
+
+  },
+  textTwoStyle: {
+    borderWidth: 3,
+    borderColor: "red",
+    flex: 1,
+    alignSelf:"flex-end"
+
+  },
+  textThreeStyle: {
+    borderWidth: 3,
+    borderColor: "red",
+    flex: 5
+
   }
 })
 
